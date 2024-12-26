b4282cfbfb51d593a8d37b989eacc01aa61d42c3
ForgTav <demo966@gmail.com>
2024-12-27 01:30:15 +0200

    Create uninstall.lua

---
+++ uninstall.lua
@@ -0,0 +1,21 @@
+local shell = require('shell')
+local scripts = {
+    'autoStat.lua',
+    'autoTier.lua',
+    'robotActions.lua',
+    'robotConfig.lua',
+    'robotGPS.lua',
+    'robotSetup.lua',
+    'start.lua',
+    'sysConfig.lua',
+    'sysDB.lua',
+    'sysGPS.lua',
+    'sysSetup.lua',
+    'uninstall.lua'
+}
+
+-- UNINSTALL
+for i=1, #scripts do
+    shell.execute(string.format('rm %s', scripts[i]))
+    print(string.format('Uninstalled %s', scripts[i]))
+end
