347ace97688cba200f1fe4779a7922539f2d0c9c
ForgTav <demo966@gmail.com>
2025-09-07 15:37:46 +0300

    Update uninstall.lua

--- uninstall.lua
+++ uninstall.lua
@@ -1,19 +1,16 @@
 local shell = require('shell')
 local scripts = {
-    'autoStat.lua',
-    'autoTier.lua',
-    'autoSpread.lua',
     'robotActions.lua',
     'robotConfig.lua',
     'robotGPS.lua',
-    'robotSetup.lua',
     'start.lua',
+    'uninstall.lua',
+    'main.lua',
     'sysConfig.lua',
     'sysDB.lua',
-    'sysGPS.lua',
-    'sysSetup.lua',
-    'main.lua',
     'sysFunction.lua',
+    'sysGPS.lua',
+    'sysLogic.lua',
     'sysUI.lua',
     'uninstall.lua'
 }
